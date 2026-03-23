6c10778465ad3fee1c79856b52b0e60cd1d99f22
dharani720 <dharaninandu20@gmail.com>
2026-03-22 21:26:12 -0500

    Update etl_pipeline.py

--- scripts/etl_pipeline.py
+++ scripts/etl_pipeline.py
@@ -1,1 +1,21 @@
+import pandas as pd
 
+# Simulated healthcare data
+data = {
+    "patient_id": [1, 2, 3],
+    "age": [45, 60, 30],
+    "billing_amount": [200, 500, 150]
+}
+
+df = pd.DataFrame(data)
+
+# Transformation
+df['billing_category'] = df['billing_amount'].apply(
+    lambda x: 'High' if x > 300 else 'Low'
+)
+
+print("Transformed Data:")
+print(df)
+
+# Save to CSV (simulate S3 upload)
+df.to_csv("processed_healthcare_data.csv", index=False)
